365a50b9f32d22677842a61906d1a8da95a042c9
avanseg <a1seg09@gmail.com>
2015-01-13 17:18:12 +0300

    Меню реализованное с помощью xml

--- app/src/main/java/su/pfm/fugithub/MainActivity.java
+++ app/src/main/java/su/pfm/fugithub/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.widget.Toast.*;
@@ -11,27 +13,46 @@ import static android.widget.Toast.*;
 
 public class MainActivity extends ActionBarActivity {
 
+    TextView textView1;
+    CheckBox checkBox1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        textView1 = (TextView) findViewById(R.id.textView1);
+        checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menu.add("menu1");
-        menu.add("menu2");
-        menu.add("menu3");
-        menu.add("menu4");
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
+       getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    // обновление меню
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        // пункты меню с ID группы = 1 видны, если в CheckBox стоит галка
+        menu.setGroupVisible(R.id.group1, checkBox1.isChecked());
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        makeText(this, item.getTitle(), LENGTH_SHORT).show();
+        StringBuilder sb = new StringBuilder();
+
+        // Выведем в TextView информацию о нажатом пункте меню
+        sb.append("Item Menu");
+        sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
+        sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
+        sb.append("\r\n order: " + String.valueOf(item.getOrder()));
+        sb.append("\r\n title: " + item.getTitle());
+        textView1.setText(sb.toString());
         return super.onOptionsItemSelected(item);
     }
 }
