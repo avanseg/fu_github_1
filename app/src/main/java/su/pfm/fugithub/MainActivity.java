5c6864762f76240cba62cfb4c91af1f9756656b3
avanseg <a1seg09@gmail.com>
2015-01-13 18:27:39 +0300

    контекстное меню

--- app/src/main/java/su/pfm/fugithub/MainActivity.java
+++ app/src/main/java/su/pfm/fugithub/MainActivity.java
@@ -1,9 +1,13 @@
 package su.pfm.fugithub;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +20,8 @@ public class MainActivity extends ActionBarActivity {
     TextView textView1;
     CheckBox checkBox1;
 
+    final int MENU_COLOR_RED = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,9 @@ public class MainActivity extends ActionBarActivity {
 
         textView1 = (TextView) findViewById(R.id.textView1);
         checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+
+        registerForContextMenu(textView1);
+        //registerForContextMenu(checkBox1);
     }
 
 
@@ -54,5 +63,29 @@ public class MainActivity extends ActionBarActivity {
         sb.append("\r\n title: " + item.getTitle());
         textView1.setText(sb.toString());
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onCreateContextMenu(ContextMenu menu, View v,
+    ContextMenu.ContextMenuInfo menuInfo) {
+       // menu.add(0,1,0,"red");
+       // menu.add(0,2,0,"blue");
+       // menu.add(0,3,0,"green");
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_red: textView1.setTextColor(Color.RED);
+                textView1.setText("Text color = red");
+                break;
+            case R.id.menu_blue: textView1.setTextColor(Color.BLUE);
+                textView1.setText("Text color = blue");
+                break;
+            case R.id.menu_green: textView1.setTextColor(Color.GREEN);
+                textView1.setText("Text color = green");
+                break;
+        }
+        textView1.setText(String.valueOf(item.getItemId()));
+        return super.onContextItemSelected(item);
     }
 }
